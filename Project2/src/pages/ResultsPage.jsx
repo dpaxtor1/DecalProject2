3bdcbf1c69bd9aa4dd11df8e15f185476baa86fd
Sara Hanmonty <anoutsala@berkeley.edu>
2024-11-15 13:54:25 -0800

    created and edited resultspage.jsx

--- Project2/src/pages/ResultsPage.jsx
+++ Project2/src/pages/ResultsPage.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function ResultsPage() {
+  return (
+    <div>
+      <h1>Generated Results</h1>
+      <p>Here will be your generated results or data.</p>
+    </div>
+  );
+}
+
+export default ResultsPage;
 
